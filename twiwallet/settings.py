55988d4ba7b475b31a3ebbc27bbae9e3e889f7a6
Taras Nek <mr.taras1k@gmail.com>
2012-03-13 01:18:00 +0200

    test

--- twiwallet/settings.py
+++ twiwallet/settings.py
@@ -167,7 +167,7 @@ LOGIN_URL = '/login/'
 AUTH_PROFILE_MODULE = 'users.Profile'
 
 TWITTER_CONSUMER_KEY = 'your key'
-TWITTER_CONSUMER_SECRET_KEY = 'your key'
+TWITTER_CONSUMER_SECRET_KEY = 'your key '
 TWITTER_REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 TWITTER_ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
 TWITTER_AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authenticate'
